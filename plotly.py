3a5fe5e989dadaf9a7e51dfb8ab9a27af9279206
EduardoMoreira25 <125419716+EduardoMoreira25@users.noreply.github.com>
2023-02-15 01:31:01 +0000

    Add files via upload

---
+++ plotly.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import base64
+from io import BytesIO
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+def generate_excel_download_link(df):
+    towrite = BytesIO()
+    df.to_excel(towrite, encoding="utf-8", index=False, header=True)  # write to BytesIO buffer
+    towrite.seek(0)  #reset pointer
+    b64 = base64.b64encode(towrite.read()).decode()
+    href = f'<a href="data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,{b64}" download="data_download.xlsx">Download Excel File</a>'
+    return st.markdown(href, unsafe_allow_html=True)
+
+def generate_html_download_link(fig):
+    towrite = StringIO()
+    fig.write_html(towrite, include_plotlyjs="cdn")
+    towrite = BytesIO(towrite.getvalue().encode())
+    b64 = base64.b64encode(towrite.read()).decode()
+    href = f'<a href="data:text/html;charset=utf-8;base64, {b64}" download="plot.html">Download Plot</a>'
+    return st.markdown(href, unsafe_allow_html=True)
+
+st.set_page_config(page_title="Excel Plotter")  ##tab
+st.title("Excel Plotter 📊") 
+st.subheader("Give me your excel file")
+
+uploaded_file = st.file_uploader("Choose a XLSX file", type="xlsx")
+## Once the user loads a file streamlit will return the file object otherwise None
+if uploaded_file:
+    st.markdown('---')
+    df = pd.read_excel(uploaded_file, engine="openpyxl")
+    st.dataframe(df)
+    groupby_column = st.selectbox(
+        "What would you like to analyse?",
+        ("Ship Mode", "Segment", "Category", "Sub-Category")  # must be 100% the same as the excel file
+    )
+
+## Group DataFrame
+
+    output_columns = ["Sales", "Profit"]
+# create new dataframe with Excel data grouped by user input
+    df_grouped = df.groupby(by=[groupby_column],as_index=False)[output_columns].sum()
+
+
+    #Plot dataframe
+    fig = px.bar(
+        df_grouped,
+        x=groupby_column,
+        y="Sales",
+        color_continuous_scale=["red","yellow","green"],
+        template="plotly_white",
+        title=f'<b>Sales & Profit by {groupby_column}</b>'
+    )
+    st.plotly_chart(fig)
+
+    ## Download Section
+    st.subheader("Downloads:")
+    generate_excel_download_link(df_grouped)
+    generate_html_download_link(fig)
